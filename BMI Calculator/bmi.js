1e358e6809f04603d6e50c2125040eaa7a7a3b06
Francis Enemuo <francenemuo@gmail.com>
2022-12-06 14:44:47 +0100

    fix: change the output result to be two decimal places

--- BMI Calculator/bmi.js
+++ BMI Calculator/bmi.js
@@ -2,7 +2,8 @@ const heading = document.querySelector('.state')
 const weight = document.querySelector('input[name="weight"]')
 const height = document.querySelector('input[name="height"]')
 const btn = document.querySelector('input[type="button"]')
-
+const span = document.querySelector('.state span')
+heading.style.display = 'none'
 
 function selectme(){
     return alert('hello')
@@ -11,9 +12,8 @@ btn.addEventListener('click', ()=> {
     let val1 = Number(weight.value)
     let val2 = Number(height.value)
     let bmi = val1/(val2*val2)
-
-    alert(bmi)
+    heading.style.display = 'block'
+    span.textContent = bmi.toFixed(2)
 }
 )
 
-heading.textContent = 'obesity'
