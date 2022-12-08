75c362d75d625e2593233f1391e3d1c2ad51f502
Francis Enemuo <francenemuo@gmail.com>
2022-12-08 16:14:54 +0100

    feat: include the character set in the function

---
+++ Password Generator/pass.js
@@ -0,0 +1,8 @@
+const six =  document.querySelector('input[name="six"]')
+const eight =  document.querySelector('input[name="eight"]')
+const ten =  document.querySelector('input[name="ten"]')
+
+const btn =  document.querySelector('button')
+const para =  document.querySelector('p')
+
+
