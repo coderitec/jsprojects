ea2152755298eb3b9f563dd800f295dd57fe8e78
Francis Enemuo <francenemuo@gmail.com>
2022-12-09 18:41:40 +0100

    fix: fix bug not working on button click

--- palindrom/pal.js
+++ palindrom/pal.js
@@ -1,16 +1,19 @@
-const pal = document.querySelector('input[type="text"]').value
 const btn = document.querySelector('input[type="button"]')
-const regex = /[a-zA-Z0-9]/
-const para = document.querySelector('p')
-
 const checkPal = () =>{
+let pal = document.querySelector('input[type="text"]')
+const regex = /[a-zA-Z0-9]/
+const para = document.querySelectorAll('p')[1]
+pal = pal.value
+pal = pal.toLowerCase()
+
     let parr = []
-    for(let x = 0; x < pal.lenght; x++){
+    for(let x = 0; x < pal.length; x++){
         if(regex.test(pal[x])){
             parr.push(pal[x])
         }
     }
     let parr2 = parr.slice()
+    console.log(parr)
     para.textContent = parr2.reverse().join('') === parr.join('')
 
 }
