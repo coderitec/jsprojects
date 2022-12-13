d451b330a11e76b122158aa0f4beef4c391d21b7
Francis Enemuo <francenemuo@gmail.com>
2022-12-13 17:11:24 +0100

    fix: complete implementation on the app

--- roman/rom.js
+++ roman/rom.js
@@ -1,36 +1,19 @@
-function convertToRoman(num) {
-    let units = ['','I','II','III','IV','V','VI','VII','VIII','IX'];
-    let tens = ['','X','XX','XXX','XL','L','LX','LXX','LXXX','XC'];
-    let hundreds = ['','C','CC','CCC','CD','D','DC','DCC','DCCC','CM'];
-    let thousands = ['','M','MM','MMM','MV','V'];
-    
-    num = num.toString();
+const num = document.getElementById('num')
+const btn = document.querySelector('input[type="button"]')
+const para = document.querySelector('p')
+
+function convertToRoman() {
+    let val = num.value 
+    let un = ['','I','II','III','IV','V','VI','VII','VIII','IX'];
+    let te = ['','X','XX','XXX','XL','L','LX','LXX','LXXX','XC'];
+    let hu = ['','C','CC','CCC','CD','D','DC','DCC','DCCC','CM'];
+    let th = ['','M','MM','MMM'];
+
+    val.length === 4 && Number(val[0])<= 3 ? val = th[val[0]]+hu[val[1]]+te[val[2]]+un[val[3]]:
+    val.length === 3 ? val = hu[val[0]]+te[val[1]]+un[val[2]]:
+    val.length === 2 ? val = te[val[0]]+un[val[1]]:val = un[val]
   
-    if (num.length === 4) {
-        let thtt = thousands[Number(num[0])];
-        let hhhh = hundreds[Number(num[1])];
-        let tttt = tens[Number(num[2])];
-        let uuuu = units[Number(num[3])];
-  
-        num = thtt + hhhh + tttt + uuuu;
-    }
-    
-   else if (num.length === 3) {
-        let hhh = hundreds[Number(num[0])];
-        let ttt = tens[Number(num[1])];
-        let uuu = units[Number(num[2])];
-        
-        num = hhh + ttt + uuu;
-      }
-     else if (num.length === 2) {
-        let tt = tens[Number(num[0])];
-        let uu = units[Number(num[1])];
-        
-        num = tt + uu;
-      }
-  
-    else{
-      num = units[Number(num[0])];
-    }
-   return num;
+   para.textContent = val
   }
+btn.addEventListener('click', convertToRoman)
+
